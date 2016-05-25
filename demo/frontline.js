76a5a163876ff58476174fa12da896957c96e2a9
Guilherme Jaccoud <guigo2k@guigo2k.com>
2016-05-24 22:00:44 -0300

    v0.6.0

---
+++ demo/frontline.js
@@ -0,0 +1,57 @@
+
+// Author: Guilherme Jaccoud <guigo@tapioca.ws>
+// Author URI: https://tapioca.ws
+// Description: Chaordic Frontline
+// Version: 0.6.0
+
+var js1 = $.getScript('//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js');
+var js2 = $.getScript('//cdn.jsdelivr.net/mustache.js/2.2.1/mustache.min.js');
+var js3 = $.getScript('//cdn.jsdelivr.net/jquery.truncate/0.1/jquery.truncate.min.js');
+
+function frontline() {
+  $.when(js1,js2,js3)
+   .then(function () {
+     $('.chaordic-container').each(function () {
+      var id = '#' + $(this).attr('id');
+      var endpoint = $(this).attr('endpoint');
+      var template = $(this).attr('template');
+      $.when(
+        $.get(template),
+        $.getJSON(endpoint)
+      ).then(function (template,response) {
+
+        // Discounts (does the platform handle this?)
+        var reference = response[0].data.reference;
+        var recommendation = response[0].data.recommendation;
+        $.each([reference,recommendation], function() {
+          $.each(this, function(i,item) {
+            onsiteDiscount(item);
+          });
+        });
+
+        var containerTemplate = $(template[0]).filter('#onsite-container').html();
+        var referenceTemplate = $(template[0]).filter('#onsite-reference').html();
+        var recommendTemplate = $(template[0]).filter('#onsite-recommend').html();
+        var containerRender = Mustache.render(containerTemplate, response[0].data);
+        var referenceRender = Mustache.render(referenceTemplate, response[0].data);
+        var recommendRender = Mustache.render(recommendTemplate, response[0].data);
+
+        $(id).html(containerRender);
+        $(id).find('.chaordic-ref').html(referenceRender).slick();
+        $(id).find('.chaordic-rec').html(recommendRender).slick(onsiteOpt.carousel);
+        $(id).find('.chaordic-item .name').truncate(onsiteOpt.truncation);
+      });
+    });
+  });
+}
+
+function onsiteDiscount(item) {
+  if(item.oldPrice) {
+    var priceOld = item.oldPrice.replace(/\D/g,'');
+    var priceNew = item.price.replace(/\D/g,'');
+    var decrease = ( priceOld - priceNew ) / priceOld * 100;
+    item.discount = Math.floor(decrease) + '%';
+  }
+}
+
+window.addEventListener('load', frontline);
